c24829efa9d3f3d7665ef1679594db1f30109585
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-26 16:47:49 -0400

    Added the function to adding and configuring workbox plugins for a service worker and manifest file. Also added the function to add CSS loaders and babel to webpack in the webpack.config js file.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+//Adding and configuring workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
@@ -18,12 +17,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generating an HTML file from a template
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      // Generating a manifest file for the PWA
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('images', 'logo'),
+          },
+        ],
+      }),
+
+      //Injecting the custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      })
     ],
 
+    // Adding CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        //Rule for JavaScript files
+        {
+          test: /\.js/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        //Rule for CSS files
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        }
       ],
     },
   };
