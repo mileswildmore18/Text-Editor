824de8bc56e37c49fd44a2b14c44003abdf75594
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-29 16:05:36 -0400

    Fixed the Install button issue that involved a typ on deferredPrompt.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result.value);
 };
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,5 @@
 const butInstall = document.getElementById('buttonInstall');
 
-
-
 // Logic for installing the PWA
 // Adding an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
@@ -17,21 +15,22 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // Implementing a click event handler on the `butInstall` element (button)
 butInstall.addEventListener('click', async () => {
-    const eventPrompt = window.defferedPrompt
+    const eventPrompt = window.deferredPrompt
     if (!eventPrompt) {
         //The deferred prompt is not available
+        console.log("Hello")
         return;
     }
     // Showing the install prompt
     eventPrompt.prompt();
 
     // Waiting for the user to respond to the prompt
-    window.defferedPrompt = null
+    window.deferredPrompt = null
     butInstall.classList.toggle('hidden', true)
 });
 
 // Adding a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('PWA was installed', event);
-    window.defferedPrompt = null;
+    window.deferredPrompt = null;
 });
